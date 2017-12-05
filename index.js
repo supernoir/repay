4b8afa8a8da21a4bb8aa652f38dee6537acf9926
supernoir <kawohl.bentodesign@gmail.com>
2017-12-06 00:14:06 +0100

    writes some specs

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-const repay = value => {
-  return value;
+exports.checkForTypeNumber = value => {};
+
+exports.getRatesLeft = (rate, paid, total) => {
+  let remainingSum = total - paid;
+  let remainingRates = remainingSum / rate;
+  return remainingRates;
 };
 
-module.exports = { repay };
+exports.repay = value => {
+  switch (typeof value) {
+    case 'object':
+      throw Error('Wrong Type: Object');
+    case 'string':
+      throw Error('Wrong Type: String');
+    case 'number':
+      return value;
+    default:
+      return;
+  }
+};
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,9 +1,28 @@
+// libraries
 const assert = require('assert');
-const should = require('chai').should();
-const expect = require('chai').expect();
+const chai = require('chai');
+
+// modules to be tested
+const repay = require('./../index');
 
 describe('Repay', function() {
-  it('should return a value', function() {
-    should.be.a('number');
+  it('should be of type number', function() {
+    chai.expect(() => repay.repay(1337).to.be.a('number'));
+  });
+  it('should throw an error on wrong type', function() {
+    chai.expect(() =>
+      repay.repay('1337').to.throw(Error('Wrong Type: String'))
+    );
+    chai.expect(() =>
+      repay.repay(['1337']).to.throw(Error('Wrong Type: Object'))
+    );
+  });
+});
+
+describe('Calculate Rates Left', function() {
+  it('should return a single number', function() {
+    chai.expect(() => {
+      repay.getRatesLeft(200, 400, 2000).to.equal(8);
+    });
   });
 });
